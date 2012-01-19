2a22d7edbb95457ecbadc5e0a58ee946db65c9ea
Kiwiupover <kiwiupover@gmail.com>
2012-01-19 15:17:44 -0800

    Removed a few tests

--- test/functional/type_of_landing_page_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TypeOfLandingPageControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/type_of_landing_page_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TypeOfLandingPageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
