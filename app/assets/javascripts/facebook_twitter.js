1a65376a1444d1d601ba26fbf2825f68b8ffdcd8
Kiwiupover <kiwiupover@gmail.com>
2012-01-26 23:12:40 -0800

    added Facebook and twitter like buttons

--- app/assets/javascripts/facebook_twitter.js
+++ app/assets/javascripts/facebook_twitter.js
@@ -7,3 +7,19 @@
 }(document, 'script', 'facebook-jssdk')); 
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+var disqus_shortname = 'landingpageoftheday'; // required: replace example with your forum shortname
+
+(function () {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}()); 
+
+var disqus_shortname = 'landingpageoftheday';
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
