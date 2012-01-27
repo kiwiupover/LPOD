8f21a1366cca94e9c624543cd36f870247608ff9
Kiwiupover <kiwiupover@gmail.com>
2012-01-26 22:05:27 -0800

    added Facebook and twitter like buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper  
-
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Landing Page of the Day").join(" | ")
+      end
+    end
+  end
 end
